670cc39b497437f13121eb989e183c69b0b8a282
Patrick Boren <Pboren@lcs.on.ca>
2021-12-10 11:31:56 -0500

    Defined the model for my app - each card has a question and an answer. Added a list of questions

---
+++ FlashCards/Model/Card.swift
@@ -0,0 +1,51 @@
+//
+//  Card.swift
+//  FlashCards
+//
+//  Created by Patrick Boren on 2021-12-10.
+//
+
+import Foundation
+
+//Defines the information we'll track
+//"Blueprint" for a card
+struct Card {
+    let question: String
+    let answer : String
+}
+
+//Define a list of cards to quiz with
+let listOfCards = [
+
+    Card(question: "What is the \"powerhouse\" of a cell?", answer: "mitochondria")
+    
+    
+    ,
+    
+    Card(question: "What is hydrolosis", answer: "when water has broken away from the cell")
+    
+    
+    ,
+  
+    Card(question: "What is CH_2?", answer: "Methylene")
+    
+    
+    ,
+   
+    Card(question: "What must we do at the start of a sentense?", answer: "Capitalize the first letter")
+    
+    
+    ,
+    
+    
+]
+
+
+
+
+
+
+
+
+
+
